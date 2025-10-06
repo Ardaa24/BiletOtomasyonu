7a5fe80366c4ad2a4b8ae77cdf9de1b670e7cbd3
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-06 11:11:49 +0300

    Create form1.cs

---
+++ form1.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp37
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool move;
+        int mouse_x;
+        int mouse_y;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mouse_x = e.X; mouse_y = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        int id = 0;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.MovieName = txtMovieName.Text;
+            
+        }
+
+
+       
+    }
+}
